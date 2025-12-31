ec2b7f5a96e3980410fa1e96f28a55bc3d0a5346
Angger-Firlana <angger.firlana56@gmail.com>
2025-12-31 14:03:22 +0700

    commit lagi

---
+++ database/migrations/2025_12_31_062051_update_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->foreignId('role_id')->constrained('roles')->after('id');
+            $table->string('pin')->after('password');
+            $table->string('token')->nullable()->after('pin');
+            $table->boolean('is_active')->default(true)->after('token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ database/migrations/2025_12_31_065536_create_lpbj_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lpbj_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('lpbj_id')->constrained('lpbjs');
+            $table->string('media');
+            $table->strin('name');
+            $table->integer('quantity');
+            $table->article('string');
+            $table->foreignId('store_id')->constrained('stores');
+            $table->string('general_ledger');
+            $table->string('cost_center');
+            $table->string('order');
+            $table->string('information');
+            $table->string('image')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lpbj_items');
+    }
+};
